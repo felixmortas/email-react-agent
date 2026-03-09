cdcaf5b9a6432b0815143614709052eb2b65612f
felixmortas <felix.mortas@hotmail.fr>
2026-03-09 10:54:10 -0500

    migrate to langfuse prompt management

--- system_prompt.py
+++ system_prompt.py
@@ -1,4 +1,5 @@
 from langchain.messages import SystemMessage
+from langfuse_engine import langfuse
 from langchain.agents.middleware import dynamic_prompt, ModelRequest
 
 system_prompt=SystemMessage(
@@ -46,51 +47,19 @@ system_prompt=SystemMessage(
 def dynamic_system_prompt(request: ModelRequest) -> str:
     website_url = request.runtime.context["website_url"]
     state = request.state
-    system_prompt = f"""
-                You are an AI agent specialized in navigation and interaction with web pages.
-                Your role is to change the email address of the account on the given website.
-                Do not ask any question to the user.
-                Always use tools to achieve this task.
-                
-                ## WORKFLOW:
-                1) If necessary, navigate to the login page by clicking on the log in button.
-                2) Fill in username and password using fill_text_field with identifier='EMAIL' and identifier='PASSWORD'
-                3) Connect clicking the Log in button
-                4) Navigate to the user profile page by clicking on the appropriate button
-                5) Fill the actual email, new email and passowrd using fill_text_field with identifier='MAIL' or 'PASSWORD' or 'NEW_EMAIL' 
-                6) Change the email address by clicking on the appropriate button
+    system_prompt = langfuse.get_prompt("email-change", type="text")
 
-                ## ⚠️ CRITICAL - CREDENTIALS HANDLING:
-                - NEVER ask for credentials - they are AUTOMATICALLY retrieved from environment variables
-                - Use fill_text_field with identifier parameter ONLY:
-                - DO NOT pass the 'value' parameter - it's handled automatically
-                - NEVER log in with third-parties like Google, Facebook, Github, ...
+    compiled_prompt = system_prompt.compile(
+        website_url=website_url,
+        isConnectionPageReached=state.get('isConnectionPageReached', False),
+        isUsernameFilled=state.get('isUsernameFilled', False),
+        isPasswordFilled=state.get('isPasswordFilled', False),
+        isLogedIn=state.get('isLogedIn', False),
+        isUserProfilPageReached=state.get('isUserProfilPageReached', False),
+        isChangeEmailPageReached=state.get('isChangeEmailPageReached', False),
+        isActualEmailFilled=state.get('isActualEmailFilled', False),
+        isNewEmailFilled=state.get('isNewEmailFilled', False),
+        isEmailChanged=state.get('isEmailChanged', False),
+    )
 
-                ## FIELD IDENTIFICATION:
-                - The website language can be English or French
-                - Email field: look for id containing 'email', type='email' or something relevent
-                - Password field: look for id containing 'passwd', 'password', type='password' or something relevent. Sometimes the password field will only appear after clicking the sign in button a first time.
-
-                ## EXAMPLE CALLS:
-                ✅ CORRECT: fill_text_field(tag='input', identifier='email', element_id='email', element_class='account_input', element_type='email')
-                ✅ CORRECT: fill_text_field(tag='input', identifier='password', element_id='passwd', element_class='account_input', element_type='password')
-
-                ## CURRENT PROGRESS STATE:
-                - isConnectionPageReached: {state.get('isConnectionPageReached', False)}
-                - isUsernameFilled: {state.get('isUsernameFilled', False)}
-                - isPasswordFilled: {state.get('isPasswordFilled', False)}
-                - isLogedIn: {state.get('isLogedIn', False)}
-                - isUserProfilPageReached: {state.get('isUserProfilPageReached', False)}
-                - isChangeEmailPageReached: {state.get('isChangeEmailPageReached', False)}
-                - isActualEmailFilled: {state.get('isActualEmailFilled', False)}
-                - isNewEmailFilled: {state.get('isNewEmailFilled', False)}
-                - isEmailChanged: {state.get('isEmailChanged', False)}
-
-                # INSTRUCTION:
-                The URL of the website to change is: {website_url}
-                At each beginning of step, check the CURRENT PROGRESS STATE above.
-                Find the first step that is False and execute it by calling all the tools needed.
-                Once a step is done, update its state to True before using the next tool required in the same tool call.
-                Unless filling steps, upgrade the steps only after receiving the HTML content and checking that the previous action worked.
-                """
-    return system_prompt
+    return compiled_prompt
